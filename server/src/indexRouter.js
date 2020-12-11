4824981bc60ab8e144cf159b31660970e3a6c1a4
Gary Stern <gcrunkster@gmail.com>
2020-12-10 20:01:26 -0500

    Update index router

--- server/src/indexRouter.js
+++ server/src/indexRouter.js
@@ -3,11 +3,11 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.status(200).json(req.session);
+  res.status(200).json(req.user);
 });
 router.get('/dashboard', (req, res) => {
   // console.log(req.session);
-  res.status(200).json(req.session);
+  res.status(200).json(req.user);
 });
 
 module.exports = router;
