aa3cea5f4bb1da715eb2a0c58cb4bedeb5714e76
Gary Stern <gcrunkster@gmail.com>
2020-12-10 20:02:36 -0500

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,13 @@
+const path = require('path');
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 const session = require('express-session');
 const passport = require('passport');
+const redis = require('redis');
 const cookieParser = require('cookie-parser');
 const connectSequelize = require('connect-session-sequelize');
+const connectRedis = require('connect-redis');
 const db = require('./infrastructure/db/db');
 const authRouter = require('./infrastructure/auth/auth-router');
 const configPassport = require('./config/passport');
@@ -17,6 +20,8 @@ const { IS_PROD, CLIENT_URL, SESSION_COOKIE_NAME, SESSION_SECRET } = require('./
 const app = express();
 app.set('trust proxy', 1);
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 if (!IS_PROD) {
   app.use(logger('dev'));
 }
@@ -25,9 +30,12 @@ app.use(cors({ origin: CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(cookieParser());
+app.use(cookieParser());
 
-const SequelizeStore = connectSequelize(session.Store);
+const RedisStore = connectRedis(session);
+// const SequelizeStore = connectSequelize(session.Store);
+
+const redisClient = redis.createClient();
 
 app.use(
   session({
@@ -39,8 +47,7 @@ app.use(
     },
     resave: false,
     saveUninitialized: false,
-    store: new SequelizeStore({ db }),
-    proxy: true,
+    store: new RedisStore({ host: 'localhost', port: 6379, client: redisClient }),
   }),
 );
 
@@ -49,8 +56,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use((req, res, next) => {
-  console.log('req.user ', req.user);
-  console.log('cookies:', req.cookies);
+  // console.log('req.user ', req.user);
+  // console.log('cookies:', req.cookies);
   // console.log('authenticated?: ', req.isAuthenticated());
   // console.log('unauthenticated?: ', req.isUnauthenticated());
   // console.log('co');
@@ -58,8 +65,6 @@ app.use((req, res, next) => {
 });
 
 app.use('/', require('./indexRouter'));
-
-// app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', require('./routes/index'));
 // app.use('/users', require('./routes/users'));
